8c456bd69358e9f1a32b0a8b7efb187d59ecc0c9
SumantSagar73 <6073sumant@gmail.com>
2025-06-22 18:14:42 +0530

    exercise 2.12-2.15 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -13,39 +13,62 @@ const App = () => {
   const [filterTerm, setFilterTerm] = useState('')
 
   useEffect(() =>{
-    console.log('effect')
-
-    axios
-      .get('http://localhost:3001/persons').then(response =>{
-        console.log('promise fulfilled')
+    personService
+      .getAll()
+      .then(
+        response =>{
         setPersons(response.data)
-      })
+  })
   }, [])
 
   const handleFilterChange = (event) => {
     setFilterTerm(event.target.value)
   }
 
-  const filteredPerson = persons.filter(person => person.name.toLowerCase().includes(filterTerm.toLowerCase()))
+  const filteredPerson = persons.filter(person =>person.name && person.name.toLowerCase().includes(filterTerm.toLowerCase()))
   
 
   const addPerson = (event) =>{
     event.preventDefault()
 
-    const nameExists = persons.some(person => person.name === newName)
-    if(nameExists){
-      alert(`${newName} is already added to phonebook`)
+    const existingPerson = persons.find(person => person.name === newName)
+    if(existingPerson){
+      const confirmUpdate = window.confirm(    `${newName} is already added to phonebook, replace the old number with a new one?`)
+
+      if(confirmUpdate){
+        const updatedPerson = {...existingPerson, number : newNumber}
+      
+
+      personService
+        .update(existingPerson.id, updatedPerson)
+        .then(returnedPerson => {
+          setPersons(
+            persons.map(p => p.id === existingPerson.id ? returnedPerson: p)
+          )
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(error =>{
+        alert(`Information of ${newName} was already removed from the server`)
+        setPersons(persons.filter(p => p.id !== existingPerson.id))
+        })
+      }
       return
     }
     const personObject = {
       name: newName,
       number: newNumber,
-      id: String(persons.length +1),
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
+  
+
   }
 
   
@@ -57,6 +80,25 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber (event.target.value)
 
+  }
+
+  const deletePerson = (id) =>{
+    const person = persons.find(n => n.id === id)
+    if(!person) return
+
+    const confirmDelete = window.confirm(`Delete person: "${person.name}"?`)
+    if( !confirmDelete) return
+
+    personService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(n => n.id !== id))
+      })
+      .catch(() => {
+        alert(`The person "${person.name}" was already deleted on the server`)
+        setPersons(persons.filter(n => n.id !== id)) // clean up locally too
+      })
+    
   }
 
   return (
@@ -77,7 +119,10 @@ const App = () => {
         handleNumberChange={handleNumberChange}
         />
       <h2>Numbers</h2>
-      <Persons persons= {filteredPerson}/>      
+      <Persons 
+        persons= {filteredPerson}
+        deleteNote={deletePerson}
+      />      
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Persons = ({persons}) => {
+const Persons = ({persons, deleteNote}) => {
   return (
     <ul style={{listStyleType: 'none', paddingLeft: 0}}>
         {persons.map((person) =>(
-          <li key={person.id}>{person.name}  {person.number}</li>
+          <li key={person.id}>{person.name}  {person.number}
+          <button onClick={() => deleteNote(person.id)} style={{ marginLeft: '1rem', color: 'red' }}>delete</button> 
+          </li>
         ))}
       </ul>
   )
